496ff244467ef8136ed7d484effd972d14b78b17
DootLord <shaunoman840@gmail.com>
2026-07-14 00:15:38 +0100

    Messy progress but progress! We grab the counterspell URL from scryfall!

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,90 @@
+use serde::Deserialize;
+use serde::Serialize;
+use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering;
+
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CollectionSummary {
+    unique_cards: u32,
+    total_cards: u32,
+}
+
+// Scryfall URL response format
+// All non-conforming response data is ignored! (Thankfully!!)
+#[derive(Deserialize)]
+struct ImageUris {
+    normal: String
+}
+
+
+#[derive(Deserialize)]
+struct ScryfallCard {
+    image_uris: ImageUris
+}
+
+static COUNTER: AtomicI32 = AtomicI32::new(0);
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn increment() -> i32 {
+    return COUNTER.fetch_add(1, Ordering::SeqCst);
+}
+
+#[tauri::command]
+fn get_current_value() -> i32 {
+    return COUNTER.load(Ordering::SeqCst);
+}
+
+#[tauri::command]
+fn get_collection_summary() -> CollectionSummary {
+    CollectionSummary {
+        unique_cards: 12,
+        total_cards: 27,
+    }
+}
+
+#[tauri::command]
+async fn get_scryfall_url(name: &str) -> Result<String, String> {
+    let scryfall_url: String = format!("https://api.scryfall.com/cards/named?fuzzy={name}");
+
+    let client = reqwest::Client::builder()
+        .user_agent("rusty-magic/0.1")
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let response = client
+        .get(scryfall_url)
+        .header("Accept", "application/json")
+        .send()
+        .await
+        .map_err(|e| format!("Scryfall URL Get Error: {e}"))?;
+
+    let card_json = match response.json::<ScryfallCard>().await {
+        Ok(resp) => resp,
+        Err(e) => return Err(format!("Error decoding Scryfall JSON: {e}"))
+    };
+
+    Ok(card_json.image_uris.normal.to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            increment,
+            get_current_value,
+            get_collection_summary,
+            get_scryfall_url
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import Card from "./components/card";
+
+interface CollectionSummary {
+  uniqueCards: number,
+  totalCards: number
+};
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [summary, setSummary] = useState<CollectionSummary | null>(null);
+  const [value, setValue] = useState(0);
+  const [card, setCard] = useState<ICard | null>(null);
+
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  async function loadSummary() {
+    const result = await invoke<CollectionSummary>("get_collection_summary");
+
+    setSummary(result)
+  }
+
+  async function increment() {
+    const result = await invoke<number>("increment");
+    setValue(result)
+  }
+
+  async function getScryFallURL(title: string) {
+    const scryfallURL = await invoke<string>("get_scryfall_url", { name: title });
+    console.log(scryfallURL);
+    setCard({ title, url: scryfallURL });
+  }
+
+  useEffect(() => {
+    getScryFallURL("Counterspell");
+  }, [])
+
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
+      {card && <Card card={card} />}
+
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
@@ -28,6 +61,20 @@ function App() {
         </a>
       </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <button onClick={loadSummary}>Load Summary</button>
+
+      {summary && (
+        <section>
+          <p>Unique Cards: {summary.uniqueCards}</p>
+          <p>Total Cards: {summary.totalCards}</p>
+        </section>
+      )}
+
+      <button onClick={increment}>Increment</button>
+
+      <p>Current Value: {value}</p>
+
 
       <form
         className="row"
